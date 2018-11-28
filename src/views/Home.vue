cd55fec84a7879b922239dba74716ab2bd3a6873
Karolin Rafalski <kr2174@caa.columbia.edu>
2018-11-28 03:15:28 -0500

    another test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-     <h2>   home home home </h2>
+     <h2>   home view </h2>
   </div>
 </template>
 
