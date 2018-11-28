f6c6b9cc130c7c20d59e33f1205e50b947863cc3
Karolin Rafalski <kr2174@caa.columbia.edu>
2018-11-28 02:57:57 -0500

    more testing

--- server.js
+++ server.js
@@ -6,26 +6,11 @@ const app = express()
 // Allow use of Heroku's port or your own local port, depending on your environment
 const PORT = process.env.PORT || 3000
 
-const filePathOptions = {
-  root: __dirname + '/dist/'
-}
 // use public folder for static assets, like css
 app.use(express.static('dist'))
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html', filePathOptions, (err) => {
-//     if (err){
-//       console.log(__dirname)
-//       console.log('++++++++++++');
-//       console.log(err);
-//     } else {
-//       console.log('Sent', __dirname);
-//     }
-//   })
-// })
 
 // Listen
 app.listen(PORT, ()=>{
