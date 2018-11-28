aaec0c55647f0361d0029ed6f49c840d48340edf
Karolin Rafalski <kr2174@caa.columbia.edu>
2018-11-28 00:51:07 -0500

    change base url to dist

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 // Middleware
 // use public folder for static assets, like css
-app.use(express.static('public'))
+app.use(express.static('dist'))
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
